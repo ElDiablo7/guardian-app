f3f8e08e33de4bfb8556a4f6f6c09e71e5b0ea35
ElDiablo7 <zacc78@gmail.com>
2026-02-24 15:13:10 +0000

    Initial commit for separate guardian repo

--- server.js
+++ server.js
@@ -52,7 +52,6 @@ app.use((req, res, next) => {
 
 // Load static assets
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/anti-phish', express.static(path.join(__dirname, '../anti-phish')));
 
 // The TITAN & Guardian Analysis Endpoint
 app.post('/api/scan', requireAuth, (req, res) => {
